75d2bce1df64371bb84760e98d185484a7a36a1c
Galiev Ramil <galievi.f@yandex.ru>
2023-04-26 15:19:59 +0300

    media-querries(2st)

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const path = require('path')
 const nextConfig = {
   reactStrictMode: true,
   sassOptions: {
-    includePaths: [path.join(__dirname, 'src', 'styles'), path.join(__dirname, 'src', 'components'), path.join(__dirname, 'src', 'components', 'ui')],
+    includePaths: [path.join(__dirname, 'src', 'styles'), path.join(__dirname, 'src', 'components')],
   },
 }
 
